ddcb2de0c61c7a5c7f7ddf549d2127739fd35b7a
panpalli <memduhpanpalli@gmail.com>
2022-10-17 14:00:47 +1000

    Add files via upload

---
+++ what_is variable.py
@@ -0,0 +1,21 @@
+name = "Memduh"
+print(name)
+print("name")
+# print(surname)
+surname = "PANPALLI"
+print(surname)
+
+# print your name side by side 3 times...
+print(name,name,name)
+print(name*3)
+print(name *3)
+print(name + " " * 3)
+print( (name + " ") * 3)
+
+print("Hello " + name)
+print("Hello " , name)
+# print("Hello" +  + 2) TypeError: (can only concatenate str (not "int") to str)
+print("Hello" + name + "2")
+print("Hello" + " " + name + " " + "2")
+print("Hello " + name + " 2")
+print("Hello " + name + " " + surname)
